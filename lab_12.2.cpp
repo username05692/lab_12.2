ae5644df57056292c646dafa88ea7ce29d1c26c2
Anna Yelnikova <elnikovaanna3@gmail.com>
2023-12-18 01:41:34 +0200

    Add project files.

---
+++ lab_12.2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h> // підключення бібліотек
+#include<math.h>
+int main() {
+
+	double a, x, b; // оголошення змінних
+    printf("Vedit a, b ,x"); // вивід повідомлення на екран
+    scanf_s("%lf%lf5lf", &a, &b, &x);
+	if 
+
+	}
+
+	else printf("It impossible if x = %f \n", x); // умова, якщо вираз не може бути обчисленим
+
+}
